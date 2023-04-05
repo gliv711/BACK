beb20fa66bf75bf44e9eca3b70e92085aceffc69
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-04-05 16:45:30 +0200

    SURVEY-MS added

---
+++ MS/RESULTAT-MS/src/main/java/com/example/resultatms/ResultatMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.resultatms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultatMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultatMsApplication.class, args);
+	}
+
+}
