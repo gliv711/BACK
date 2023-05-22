0bc3866761b1fe84d7a9cca2f4e3d9c37a86bdd0
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-22 11:07:54 +0200

    t

--- MS/USER-MS/src/main/java/com/example/userms/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.userms.repository;
-
-import com.example.userms.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
